de930f05733afd797e09ee1fc5844f8c13c307d5
sikarwarshivam <shivamsinghsikarwar42@gmail.com>
2026-09-25 10:19:58 +0530

    feat(cart): add shopping cart route

---
+++ src/app/(store)/cart/page.tsx
@@ -0,0 +1,9 @@
+import { CartPage } from "@/components/store/cart-page";
+
+export const metadata = {
+  title: "Shopping Cart",
+};
+
+export default function CartRoute() {
+  return <CartPage />;
+}
