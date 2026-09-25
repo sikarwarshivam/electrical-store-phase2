57898d413edc3f18e4410c60a4d0360226d3953b
sikarwarshivam <shivamsinghsikarwar42@gmail.com>
2026-09-25 10:20:27 +0530

    feat(checkout): add checkout route

---
+++ src/app/(store)/checkout/page.tsx
@@ -0,0 +1,9 @@
+import { CheckoutPage } from "@/components/store/checkout-page";
+
+export const metadata = {
+  title: "Checkout",
+};
+
+export default function CheckoutRoute() {
+  return <CheckoutPage />;
+}
